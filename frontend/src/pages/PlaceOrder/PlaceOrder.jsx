b7dc1ee29961fcba2df45b680864de5c6611142b
Gautam Kumar <126412810+kumargautamsp@users.noreply.github.com>
2024-09-02 01:58:56 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
-import './Placeorder.css'; 
+import './PlaceOrder.css';
+
 
 
 import {StoreContext} from '../../context/StoreContext'
