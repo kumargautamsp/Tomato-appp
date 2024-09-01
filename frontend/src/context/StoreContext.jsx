39d044558b01b91c1a09f69fa231c63ad558806e
Gautam Kumar <126412810+kumargautamsp@users.noreply.github.com>
2024-09-02 01:50:57 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState ({})
-    const url = "https://tomato-app-5dhz.onrender.com"
+    const url = "https://tomato-appp-backend.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
